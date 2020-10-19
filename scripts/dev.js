4c5eabf3035af2c4a56f0c179b72b41d21a5ec40
Menka Kayan <menka.kayan@uspto.gov>
2020-10-19 12:17:29 -0400

    updating dev script

--- scripts/dev.js
+++ scripts/dev.js
@@ -2,8 +2,8 @@ const { renderSassFile, watch } = require('./build')
 
 const log = (...a) => console.log(...['[dev]', ...a].map(String))
 
-watch('src/assets/scss', file => {
-  renderSassFile({ file: 'src/assets/scss/main.scss' }).catch(log)  
+watch('src/scss', file => {
+  renderSassFile({ file: 'src/scss/usptostrap.scss' }).catch(log)  
 })
 
 
